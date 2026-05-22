07bb50a51050daf9d942f43c33d2c0d8b8f2c373
JoaoGaValentim <joao.gabriel.vale.theodoro@gmail.com>
2026-05-22 15:17:55 -0300

    added modifications

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -2,13 +2,20 @@
 fn main() {
     // Podemos debbugar para encontrar erros
     // em nosso código e fixa-los.
-    println!("{}", fibonacci(15));
-    println!("{}", factorial(5));
-    println!("============");
-    countdown(10, 1);
-    println!("============");
-    countdown(1, 11);
-    println!("============");
+    // Temos o conceito de ponto de parada, ou
+    // breakpoint, para interromper o código em um
+    // dado ponto.
+    // Com isso podemos verificar os valores
+    // de variáveis, execuções de funções e
+    // etc.
+    // println!("{}", fibonacci(15));
+    // println!("{}", factorial(5));
+    // println!("============");
+    // countdown(10, 1);
+    // println!("============");
+    // countdown(1, 11);
+    // println!("============");
+    generate_multiplication_table(0, 10, 3);
 }
 
 // Recursão é quando uma função
@@ -36,18 +43,34 @@ fn factorial(n: i32) -> i32 {
 }
 
 fn countdown(start: i32, end: i32) {
+    println!("{start}");
     if start == end {
-        println!("{start}");
         return;
     }
 
     if start > end {
-        println!("{start}");
         countdown(start - 1, end);
     }
 
     if end > start {
-        println!("{start}");
         countdown(start + 1, end);
+    }
+}
+
+fn generate_multiplication_table(start: i32, end: i32, number: i32) {
+    let value: i32 = start * number;
+
+    println!("{start} x {number} = {value}");
+
+    if start == end {
+        return;
+    }
+
+    if start > end {
+        generate_multiplication_table(start - 1, end, number);
+    }
+
+    if end > start {
+        generate_multiplication_table(start + 1, end, number);
     }
 }
