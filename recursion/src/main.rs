709bc92ed844a591b403534208fcaf284e85fc1f
JoaoGaValentim <joao.gabriel.vale.theodoro@gmail.com>
2026-05-22 21:23:40 -0300

    added modifications

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -8,54 +8,57 @@ fn main() {
     // Com isso podemos verificar os valores
     // de variáveis, execuções de funções e
     // etc.
-    // println!("{}", fibonacci(15));
-    // println!("{}", factorial(5));
-    // println!("============");
-    // countdown(10, 1);
-    // println!("============");
-    // countdown(1, 11);
-    // println!("============");
+    println!("===========");
     generate_multiplication_table(0, 10, 3);
+    println!("===========");
+    generate_multiplication_table(0, 10, 2);
+    println!("===========");
+    generate_multiplication_table(0, 10, 4);
+    println!("===========");
+    generate_multiplication_table(0, 10, 1000);
+    println!("===========");
+    generate_multiplication_table(0, 10, 1_000_000);
+    println!("===========");
 }
 
 // Recursão é quando uma função
 // chama a si mesma.
 // Todas possuem um caso base, uma condição
 // de parada. Ela impede outras auto-invokes da mesma.
-fn fibonacci(value: usize) -> usize {
-    if value == 0 {
-        return 0;
-    }
+// fn fibonacci(value: usize) -> usize {
+//     if value == 0 {
+//         return 0;
+//     }
 
-    if value == 1 {
-        return 1;
-    }
+//     if value == 1 {
+//         return 1;
+//     }
 
-    fibonacci(value - 1) + fibonacci(value - 2)
-}
+//     fibonacci(value - 1) + fibonacci(value - 2)
+// }
 
-fn factorial(n: i32) -> i32 {
-    if n <= 0 {
-        return 1;
-    }
+// fn factorial(n: i32) -> i32 {
+//     if n <= 0 {
+//         return 1;
+//     }
 
-    n * factorial(n - 1)
-}
+//     n * factorial(n - 1)
+// }
 
-fn countdown(start: i32, end: i32) {
-    println!("{start}");
-    if start == end {
-        return;
-    }
+// fn countdown(start: i32, end: i32) {
+//     println!("{start}");
+//     if start == end {
+//         return;
+//     }
 
-    if start > end {
-        countdown(start - 1, end);
-    }
+//     if start > end {
+//         countdown(start - 1, end);
+//     }
 
-    if end > start {
-        countdown(start + 1, end);
-    }
-}
+//     if end > start {
+//         countdown(start + 1, end);
+//     }
+// }
 
 fn generate_multiplication_table(start: i32, end: i32, number: i32) {
     let value: i32 = start * number;
